a188968823bbec3edbeb0097f68193a65bfeae55
taruni-14 <30998560+taruni-14@users.noreply.github.com>
2023-12-13 15:31:37 +0530

    development day 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Footer.css'
+import {ReactComponent as PlayIcon} from '../assets/PlayIcon.svg';
+import {ReactComponent as NextIcon} from '../assets/NextIcon.svg';
+import {ReactComponent as PauseIcon} from '../assets/PauseIcon.svg';
+import {ReactComponent as PreviousIcon} from '../assets/PreviousIcon.svg';
 
 export default function Footer() {
+  const [isPlaying, setIsPlaying] =useState(false)
   return (
-    <div>footer</div>
+    <div className='footer-container'>
+      <div className='player-control'>
+        <PreviousIcon width={'32px'} />
+          {/* for play icon */}
+        {
+          isPlaying
+          ?
+          <PauseIcon width={'32px'} onClick={()=>setIsPlaying(false)} />
+          :
+          <PlayIcon width={'32px'}  onClick={()=>setIsPlaying(true)} />
+        }
+        <NextIcon width={'32px'} />
+      </div>
+    </div>
   )
 }
 
--- src/components/PlayerPlaylist.js
+++ src/components/PlayerPlaylist.js
@@ -4,57 +4,108 @@ import './PlayerPlaylist.css'
 const mySinger=[
   {
     id:1,
-    singername:'Shreya Goshal',
+    artist:'Shreya Goshal',
+    title:'piyu bhole',
+    duration: '3:45'
   },
   {
     id:2,
-    singername:'Shankar Mahadevan',
+    artist:'Shankar Mahadevan',
+    title:'Breathless',
+    duration: '2:45'
   },
   {
     id:3,
-    singername:'Sonu Nigam',
+    artist:'Sonu Nigam',
+    title:'jeelu zara',
+    duration: '3:45'
   },
   {
     id:4,
-    singername:'Ariana Grande',
+    artist:'Ariana Grande',
+    title:'7 rings',
+    duration: '3:45'
   },
   {
     id:5,
-    singername:'Taylor Swift',
+    artist:'Ellie Goulding',
+    title:'Army',
+    duration: '3:45'
   },
   {
     id:6,
-    singername:'Ellie Goulding',
+    artist:'Alan Walker',
+    title:'Alone',
+    duration: '3:45'
   },
   {
     id:7,
-    singername:'Billie Elish',
+    artist:'Billie ellish',
+    title:'Lovely',
+    duration: '3:45'
   },
   {
     id:8,
-    singername:'Salena Gomez',
+    artist:'Taylor swift',
+    title:'bad blood',
+    duration: '3:45'
   },
   {
     id:9,
-    singername:'Alan Walker',
+    artist:'zoya',
+    title:'beautiful',
+    duration: '3:45'
   },
   {
     id:10,
-    singername:'Gabriel',
+    artist:'Gabriel',
+    title:'Miss you',
+    duration: '3:45'
   },
 ]
 export default function PlayerPlaylist() {
   return (
     <div className='playlist-container'>
-    <div className='player-playlist-names'>
-      {
-      mySinger.map((items)=>{
-        <div className='player-playlist-items' key={items.id}>
-          {items.singername}
+    <div className='player-playlist-header'>
+      <div className='playlist-image-card'>
+
+      </div>
+      <div className='playlist-body'>
+      <p>PLAYLIST</p>
+      <h1>Best of mine</h1>
+      </div>
+      </div>
+      <div className='playlist-list'>
+        <div className='playlist-list-id'>
+          #
         </div>
-      })
-    }
-    </div>
-    </div>
+        <div className='playlist-list-artist'>
+          ARTIST
+        </div>
+        <div className='playlist-list-title'>
+          TITLE
+        </div>
+        <div className='playlist-list-duration'>
+          DURATION
+        </div>
+      </div>
+      <div className='playlist-list-container'>
+        {
+          mySinger.map((items)=>{
+            return(
+            <div className='playlist-list'>
+              <div className='playlist-list-id'>{items.id}</div>
+            <div className='playlist-list-artist'>{items.artist}</div>
+            <div className='playlist-list-title'>{items.title}</div>
+            <div className='playlist-list-duration'>{items.duration}</div>
+            </div>
+            
+          )})
+  
+        }
+      </div>
+  </div>
+    
+    
   )
 }
